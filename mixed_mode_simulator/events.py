bcfb0cf849db92c165531918bf2dc0cea66db362
prince <prince.apple.john@gmail.com>
2024-04-19 00:32:00 -0500

    Analog buffer can allocate downstream buffers.

--- mixed_mode_simulator/events.py
+++ mixed_mode_simulator/events.py
@@ -1,11 +1,14 @@
+import simpy
+
+
 class DetectionEvent:
-    def __init__(self, event, event_info):
+    def __init__(self, event: simpy.Event, event_info):
         self.event = event
         self.event_info = event_info
 
 
 class DownstreamEvent:
-    def __init__(self, event, detection_event_info, event_info):
+    def __init__(self, event: simpy.Event, detection_event_info, event_info, final_event= False):
         """
         This defines the events that the integrator dispatches after the arrival of a detection event.
         :param event: Simpy Event
@@ -15,3 +18,4 @@ class DownstreamEvent:
         self.event = event
         self.detection_event_info = detection_event_info
         self.event_info = event_info
+        self.final_event = final_event
