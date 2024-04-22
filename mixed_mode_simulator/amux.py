975207756f02494a6040a0a889df3eb669ce640d
prince <prince.apple.john@gmail.com>
2024-04-21 23:10:09 -0500

    AMUX should work now. Minor changes to descriptions of AnalogBuffer

--- mixed_mode_simulator/amux.py
+++ mixed_mode_simulator/amux.py
@@ -7,41 +7,53 @@ the tail buffers."""
 
 
 class AMUX:
-    def __init__(self, env, channels, upstream_buffer, downstream_buffers: list, debug=False):
+    def __init__(self, env: simpy.Environment, channels: int, downstream_buffers: list, amux_delay=0, debug=False):
+        """
+        Analog MUX simulation model. Takes in a list of downstream buffers and stores them in a Simpy Store FIFO.
+        Downstream buffers are allocated until they are present in the FIFO. If FIFO is empty the entire event is
+        dropped.
 
-        self.current_tail_buffer = 0
+        :param amux_delay: Constant delay incurred due to switching costs, default = 0.
+        :param env: Simpy Environment
+        :param channels: number of upstream channels
+        :param downstream_buffers: List of Buffer objects that are downstream
+        :param debug: Debug flag, default False
+        """
+        self.amux_delay = amux_delay
         self.env = env
         self.channels = channels
         self.active_channels = {}  # dict of type {channel_index: buffer_resource}
         self.num_downstream_buffers = len(downstream_buffers)
         self.downstream_buffers = downstream_buffers
-        self.downstream_resource_manager = simpy.Resource(self.env, capacity=self.num_downstream_buffers)
-
         self.downstream_buffer_fifo = simpy.Store(self.env, capacity=self.num_downstream_buffers)
         self.debug = debug
 
         self.fill_fifo(self.downstream_buffers)
 
-    def fill_fifo(self, buffers: list):
+    def fill_fifo(self, buffers: list[AnalogBuffer]):
         for buffer in buffers:
             self.downstream_buffer_fifo.put(buffer)
 
-    def acquire_buffer(self, upstream_channel: int) -> simpy.Resource:
+    def acquire_buffer(self, upstream_channel: int) -> AnalogBuffer:
 
-        if self.downstream_resource_manager.count < self.downstream_resource_manager.capacity:
-            buffer_request = self.downstream_resource_manager.request()
+        if len(self.downstream_buffer_fifo.items) > 0:
+            buffer = self.downstream_buffer_fifo.get()
             if self.debug:
-                print(f'Downstream buffer {self.downstream_resource_manager.count - 1} '
+                print(f'Downstream buffer {buffer.buffer_index} '
                       f'available for upstream channel {upstream_channel} and acquired at {self.env.now}')
-            return buffer_request
+            return buffer
         else:
             raise BufferError
 
+    def release_buffer(self, channel_index):
+        buffer = self.active_channels[channel_index]["buffer"]
+        self.downstream_buffer_fifo.put(buffer)  # No yield because i do not see any possible system state where the
+        # fifo will be full.
+
     def accept_event(self, event: DownstreamEvent, channel_index: int):
 
-        next_buffer = self.active_channels[channel_index]["b"]
-
-        pass
+        downstream_buffer = self.active_channels[channel_index]["buffer"]
+        yield self.env.process(downstream_buffer.buffer(event))
 
     def drop_event(self, event):
         if self.debug:
@@ -50,23 +62,23 @@ class AMUX:
                   f'dropped. No downstream channels available')
         event.event.fail()
 
-    def entry_point(self, channel_index: int, event: DownstreamEvent, ):
+    def entry_point(self, channel_index: int, event: DownstreamEvent):
 
         print("Got an event")
         if event.event_info["sample_index"] == 0:
+            yield self.env.timeout(self.amux_delay)
             try:
-                buffer_resource = self.acquire_buffer(channel_index)
-                self.active_channels.append(
-                    {channel_index: {"buffer_resource": buffer_resource, "tail_buffer": self.current_tail_buffer}})
+                buffer = self.acquire_buffer(channel_index)
+                self.active_channels[channel_index] = {"buffer": buffer}
             except BufferError as e:
                 print("No buffers available, event dropped")
-        elif any(channel == channel_index for channel, _ in self.active_channels):
+        elif channel_index in self.active_channels:
             """
             If from active channel, pass on to next downstream buffer.
             """
+            yield self.accept_event(event, channel_index)
             if event.final_event:
-                self.downstream_resource_manager.release(self.active_channels[channel_index]["buffer_resource"])
-            self.accept_event(event, channel_index)
+                self.release_buffer(channel_index)
         else:
             """
             Not in active channel. Event is dropped. 
--- mixed_mode_simulator/sample_and_hold.py
+++ mixed_mode_simulator/sample_and_hold.py
@@ -6,11 +6,13 @@ from .events import *
 
 class AnalogBuffer:
 
-    def __init__(self, env, sample_length, buffer_length, chain_delay, debug=False):
+    def __init__(self, env, buffer_index, buffer_location, sample_length, buffer_length, chain_delay, debug=False):
         """
         Creates an analog buffer with a parameterizable number of individual memory units and sample lengths.
         This class can be used to create both the long tail buffers and initial ring buffers.
 
+        :param buffer_index: unique index count of the buffer in its respective location.
+        :param buffer_location: String either "ring" or "tail" to determine the location of this buffer object
         :param env: simpy environment
         :param buffer_length: number of chained S&H units
         :param sample_length: amount of time taken per sample unit
@@ -18,6 +20,8 @@ class AnalogBuffer:
         :param debug: Boolean flag for printing debug messages, default = `False`
 
         """
+        self.buffer_index = buffer_index
+        self.buffer_location = buffer_location
         self.env = env
         self.sample_length = sample_length
         self.buffer_length = buffer_length
@@ -68,3 +72,5 @@ class AnalogBuffer:
             yield self.env.timeout(self.chain_delay)  # Adding chaining delay overhead
 
         self.remove_from_buffer(event, None)
+
+
